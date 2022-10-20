5dba9ee758838e6e1fe16968d4ef5ec906b623f9
Rafael Cassaro <rafaelcassacosta@hotmail.com>
2022-10-20 11:25:40 -0300

    Payment, one-to-one association

--- src/main/java/com/unitycourse/course/config/TestConfig.java
+++ src/main/java/com/unitycourse/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.unitycourse.course.entities.Category;
 import com.unitycourse.course.entities.Order;
 import com.unitycourse.course.entities.OrderItem;
+import com.unitycourse.course.entities.Payment;
 import com.unitycourse.course.entities.Product;
 import com.unitycourse.course.entities.Usuario;
 import com.unitycourse.course.entities.enums.OrderStatus;
@@ -81,6 +82,11 @@ public class TestConfig implements CommandLineRunner { //CommandLineRunner é pr
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 
